4bea579b7974c6a4e0146c4ec64b1761d11c1956
SymWhy <simone.whysel@gmail.com>
2022-02-17 02:07:14 -0500

    add buildGrid() function

---
+++ sudoku.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+function buildGrid() {
+    let digits = [11,22,33,44,55,66,77,88,99];
+    let myGrid = [];
+
+    for (let i = 0; i < 9; i++) {
+        x= getRandomInt(digits.length);
+        let n = digits[x];
+        myGrid.push(n);
+        digits.splice(x,1);
+    }
+    //return digits;
+    return myGrid;
+}
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomInt(n) {
+    return Math.floor(Math.random() * n);
+}
