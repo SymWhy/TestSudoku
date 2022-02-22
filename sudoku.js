6abade0e46a1441434bf54c473ada616fc7c9f0a
SymWhy <simone.whysel@gmail.com>
2022-02-22 14:26:18 -0500

    randomize digits in myGrid

--- sudoku.js
+++ sudoku.js
@@ -15,14 +15,16 @@ function setupGrid() {
 
 function buildGrid() {
     myGrid = []; //clear myGrid
-    for (let i = 0; i < 9; i++) {
-        x= getRandomInt(digits.length);
-        let n = digits[x];
+    unTrash = [11,22,33,44,55,66,77,88,99];
+    for (let i = 0; i < digits.length; i++) {
+        x = getRandomInt(unTrash.length);
+        let n = unTrash[x];
+        
         myGrid.push(n);
-        digits.splice(x,1);
+        unTrash.splice(x,1);
     }
     //return digits;
-    return myGrid;
+    setupGrid();
 }
 
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
