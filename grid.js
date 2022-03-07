2b5770285b6896ff1b96c26c58d88712ead41bc3
SymWhy <simone.whysel@gmail.com>
2022-03-07 16:41:52 -0500

    standalone 3x3 that outputs an array

---
+++ grid.js
@@ -0,0 +1,17 @@
+//Javascript source code
+
+const grid9x9 = document.getElementById('grid9x9');
+
+let subgridList = [];
+
+function buildSubgrid() {
+    myDigit = '☐';
+    for (let i = 0; i < 9; i++) {
+        let mySubgrid = document.createElement('div');
+        //mySubgrid.class = 'numbox';
+        mySubgrid.id = 'subgrid' + i;
+        mySubgrid.innerText = myDigit;
+            grid9x9.appendChild(mySubgrid);
+            subgridList.push(mySubgrid);
+    }
+}
