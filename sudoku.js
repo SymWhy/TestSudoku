2431c433b4a00df0eae962c0609e11336332599f
SymWhy <simone.whysel@gmail.com>
2022-03-10 10:22:56 -0500

    remove references to HTML elements for now

--- sudoku.js
+++ sudoku.js
@@ -2,14 +2,9 @@
 let digits = [11,22,33,44,55,66,77,88,99];
 let myGrid = digits; //current grid in numerical order
 
-let numbers = document.getElementsByClassName('boxes'); //returns an array of HTML elements
 let button = document.getElementById('shuffler');
 
-//grab div containing all 9 boxes
-const sudokuBox = document.getElementById('sudoku');
-
 window.onload = () => {
-    build3x3();
     setupGrid();
 }; //populate grid when window loads
 
@@ -19,10 +14,10 @@ button.addEventListener('click', () => {
 });
 
 function setupGrid() {
-    //consider generating boxes in js instead of html
-    for (let i = 0; i < numbers.length; i++) {
-        numbers[i].innerHTML = myGrid[i];
+    for (let i = 0; i < digits.length; i++) {
+        //placeholder
     }
+    console.log(digits);
 }
 
 function buildGrid() {
@@ -37,22 +32,6 @@ function buildGrid() {
     }
     //return digits;
     setupGrid();
-}
-
-function build3x3() {
-    
-    //sudokuBox.appendChild(child);
-    for (let i = 0; i < digits.length; i++) {
-        //create a new div
-        let myElement = document.createElement('div');
-        myElement.className = 'boxes';
-        //generate div id based on location in array
-        myElement.id = 'box' + i;
-        myElement.innerText = digits[i];
-        //add div to sudoku box
-        sudokuBox.appendChild(myElement);
-    }
-    //now we have a series of nine divs
 }
 
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
