17e3cd949377b5b808a8e177fd3ec4450efb541f
SymWhy <simone.whysel@gmail.com>
2022-03-10 10:29:00 -0500

    make myGrid local to buildGrid()

--- sudoku.js
+++ sudoku.js
@@ -1,6 +1,5 @@
 // JavaScript source code
 let digits = [11,22,33,44,55,66,77,88,99];
-let myGrid = digits; //current grid in numerical order
 
 let button = document.getElementById('shuffler');
 
@@ -21,8 +20,8 @@ function setupGrid() {
 }
 
 function buildGrid() {
-    myGrid = []; //clear myGrid
-    unTrash = [11,22,33,44,55,66,77,88,99];
+    myGrid = []; //holder for new shuffled array
+    unTrash = [11,22,33,44,55,66,77,88,99]; //move digits from here to myGrid
     for (let i = 0; i < digits.length; i++) {
         x = getRandomInt(unTrash.length);
         let n = unTrash[x];
@@ -30,8 +29,7 @@ function buildGrid() {
         myGrid.push(n);
         unTrash.splice(x,1);
     }
-    //return digits;
-    setupGrid();
+    console.log(myGrid);
 }
 
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
