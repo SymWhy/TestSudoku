9eae65068514807f2d106855694a33c832753810
SymWhy <simone.whysel@gmail.com>
2022-03-23 10:59:01 -0400

    commit to new branch

---
+++ README.md
@@ -0,0 +1,10 @@
+Generate an array of 81 zeroes, grid[]
+Generate random array length 9 using digits[]
+for testing purposes just use digits[] order
+Loop 9 (Assign digits to respective grid[i])
+
+Grid = the whole sudoku grid of 3x3 subgrids
+Subgrid = 3x3 subgrid of 9 random digits
+Digit = Numbers within a subgrid, none should repeat
+
+Array = ITS AN ARRAY
--- sudoku.js
+++ sudoku.js
@@ -1,145 +1,35 @@
 // JavaScript source code
-const digits = [11,22,33,44,55,66,77,88,99];
+const digits = [1,2,3,4,5,6,7,8,9];
 
-let gridAx = [];
-let gridBx = [];
-let gridCx = [];
-
-let gridAy = [];
-let gridBy = [];
-let gridCy = [];
-
-//how do we generate variables dynamically?
-let subgridA = digits;
-let subgridB = digits;
-let subgridC = digits;
-let subgridD = digits;
-let subgridE = digits;
-let subgridF = digits;
-let subgridG = digits;
-let subgridH = digits;
-let subgridI = digits;
-const grids = [subgridA,subgridB,subgridC,subgridD,subgridE,subgridF,subgridG,subgridH,subgridI];
-
+let gridArray = [];
 let button = document.getElementById('shuffler');
 
-window.onload = () => {
-    setupGrid();
-}; //populate grid when window loads
+// window.onload = () => {
+//     setupGrid();
+// }; //populate grid when window loads
 
 //reshuffle grid on button click
 button.addEventListener('click', () => {
     buildGrid();
 });
 
-function setupGrid() {
-    for (let i = 0; i < digits.length; i++) {
-        let myGrid = grids[i];
-        console.log(myGrid);
-    }
-}
-
 function buildGrid() {
-    for (let j = 0; j < 9; j++) {
-        mySubgrid = []; //holder for new shuffled array
-        unTrash = [11, 22, 33, 44, 55, 66, 77, 88, 99]; //move digits from here to myGrid
-        for (let i = 0; i < 9; i++) {
-            x = getRandomInt(unTrash.length);
-            let n = unTrash[x];
-
-            mySubgrid.push(n);
-            unTrash.splice(x, 1);
-        }
-        //console.log(mySubgrid);
-        grids[j] = mySubgrid
-        //console.log(grids[j]);
-    }
-    //clear grid primes
-    gridAx = [];
-    gridBx = [];
-    gridCx = [];
-
-    gridAy = [];
-    gridBy = [];
-    gridCy = [];
-
-    //grab rows (x)
-    for (let i = 0; i < 9; i++) {
-        if (i < 3) {
-            gridAx.push(grids[i]);
-        }
-        else if (i < 6) {
-            gridBx.push(grids[i]);
-        }
-        else {
-            gridCx.push(grids[i]);
+    //testing, no randomization
+    for (let j = 0; j < digits.length; j++) {
+        //fill mySubgrid with 1-9
+        for (let i = 0; i < digits.length; i++) {
+            gridArray.push(digits[i]);
         }
     }
-    console.log('rows:');
-    console.log(gridAx, gridBx, gridCx);
-
-    for (let i = 0; i < 9; i++) {
-        if (i % 3 == 0){
-            gridAy.push(grids[i]);
-        }
-        else if (i % 3 == 1) {
-            gridBy.push(grids[i]);
-        }
-        else if (i % 3 == 2) {
-            gridCy.push(grids[i]);
-        }
-    }
-    console.log('columns:')
-    console.log(gridAy, gridBy, gridCy);
-
     //print sudoku grid
-    console.log(
-        '|' + 
-        gridAx[0].slice(0,3) + '|',
-        gridBx[0].slice(0,3) + '|',
-        gridCx[0].slice(0,3) + '|',
-        '\n',
-        '|' +
-        gridAx[1].slice(0,3) + '|',
-        gridBx[1].slice(0,3) + '|',
-        gridCx[1].slice(0,3) + '|',
-        '\n',
-        '|' +
-        gridAx[2].slice(0,3) + '|',
-        gridBx[2].slice(0,3) + '|',
-        gridCx[2].slice(0,3) + '|',
-        '\n' + '------------------------------' + '\n',
-        '|' + 
-        gridAx[0].slice(3,6) + '|',
-        gridBx[0].slice(3,6) + '|',
-        gridCx[0].slice(3,6) + '|',
-        '\n',
-        '|' +
-        gridAx[1].slice(3,6) + '|',
-        gridBx[1].slice(3,6) + '|',
-        gridCx[1].slice(3,6) + '|',
-        '\n',
-        '|' +
-        gridAx[2].slice(3,6) + '|',
-        gridBx[2].slice(3,6) + '|',
-        gridCx[2].slice(3,6) + '|',
-        '\n' + '------------------------------' + '\n',
-        '|' + 
-        gridAx[0].slice(6,9) + '|',
-        gridBx[0].slice(6,9) + '|',
-        gridCx[0].slice(6,9) + '|',
-        '\n',
-        '|' +
-        gridAx[1].slice(6,9) + '|',
-        gridBx[1].slice(6,9) + '|',
-        gridCx[1].slice(6,9) + '|',
-        '\n',
-        '|' +
-        gridAx[2].slice(6,9) + '|',
-        gridBx[2].slice(6,9) + '|',
-        gridCx[2].slice(6,9) + '|',
-    );
+    return gridArray;
 }
+
+function rotateArray(a,k) {
+QZ
+    return myArray;
+}
+
 
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomInt(n) {
