b42e465e0132af62187315c1d92b36bdee3ca558
SymWhy <simone.whysel@gmail.com>
2022-04-25 17:07:49 -0400

    generate outer "box" wrapper and inner "cell"

--- grid.js
+++ grid.js
@@ -6,13 +6,19 @@ function buildGrid() {
     let data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
     let grid = document.getElementById('grid');
 
-    for (let i = 0; i < data.length; i++) {
+    for (let i = 0; i < 3; i++) {
         let subdata = data[i];
-        for (let j = 0; j < subdata.length; j++) {
-            let cell = document.createElement('div');
-            cell.innerHTML = subdata[j];
-            cell.className = 'cell';
-            grid.appendChild(cell);
+        for (let j = 0; j < 3; j++) {
+            let box = document.createElement('div');
+            //box.innerHTML = subdata[j];
+            box.className = 'box';
+            for (let k = 0; k < 3; k++) {
+                let cell = document.createElement('div');
+                cell.innerHTML = subdata[k];
+                cell.className = 'cell';
+                box.appendChild(cell);
+            }
+            grid.appendChild(box);
         }
     }
 }
