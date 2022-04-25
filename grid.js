ab8c925a1ed8859c1ad0485f05e85d0c953cf255
SymWhy <simone.whysel@gmail.com>
2022-04-25 17:09:49 -0400

    each box produces 9 cells

--- grid.js
+++ grid.js
@@ -3,7 +3,7 @@
 function buildGrid() {
     //https://code-boxx.com/create-grid-javascript/
 
-    let data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    let data = [[1,2,3,4,5,6,7,8,9], [4,5,6,7,8,9,1,2,3], [7,8,9,1,2,3,4,5,6]];
     let grid = document.getElementById('grid');
 
     for (let i = 0; i < 3; i++) {
@@ -12,7 +12,7 @@ function buildGrid() {
             let box = document.createElement('div');
             //box.innerHTML = subdata[j];
             box.className = 'box';
-            for (let k = 0; k < 3; k++) {
+            for (let k = 0; k < 9; k++) {
                 let cell = document.createElement('div');
                 cell.innerHTML = subdata[k];
                 cell.className = 'cell';
