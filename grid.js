9fd48b4d1f409345098f186457b3f60ade5abd97
SymWhy <simone.whysel@gmail.com>
2022-04-25 22:15:15 -0400

    simplify to own, original code

--- grid.js
+++ grid.js
@@ -3,22 +3,22 @@
 function buildGrid() {
     //https://code-boxx.com/create-grid-javascript/
 
-    let data = [[1,2,3,4,5,6,7,8,9], [4,5,6,7,8,9,1,2,3], [7,8,9,1,2,3,4,5,6]];
+    let data = [1,2,3,4,5,6,7,8,9, 4,5,6,7,8,9,1,2,3, 7,8,9,1,2,3,4,5,6];
     let grid = document.getElementById('grid');
-
-    for (let i = 0; i < 3; i++) {
-        let subdata = data[i];
-        for (let j = 0; j < 3; j++) {
-            let box = document.createElement('div');
-            //box.innerHTML = subdata[j];
-            box.className = 'box';
-            for (let k = 0; k < 3; k++) {
-                let cell = document.createElement('div');
-                cell.innerHTML = subdata[k];
-                cell.className = 'cell';
-                box.appendChild(cell);
-            }
-            grid.appendChild(box);
+    let letters = ['A','B','C','D','E','F','G','H','I'];
+    for (let i = 0; i < 9; i++) {
+        //create nine divs
+        let box = document.createElement('div');
+        box.className = 'box';
+        box.id = 'box' + letters[i];
+        grid.appendChild(box);
+        for (let j = 0; j < 9; j++) {
+            //create nine sub-divs inside each div
+            let cell = document.createElement('div');
+            cell.className = 'cell';
+            cell.id = 'cell' + letters[i] + j;
+            cell.innerHTML = cell.id;
+            box.appendChild(cell);
         }
     }
 }
